adfb0a31a7d5fda7479e316bd927dbceb9ae290d
TANG ZHIXIONG <zhixiong.tang@momenta.ai>
2024-05-26 00:10:08 +0800

    update

--- include/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-
-DIR = os.path.abspath(os.path.dirname(__file__))
-
-
-def get_include() -> str:
-    installed_path = os.path.join(DIR, "include")
-    source_path = os.path.join(os.path.dirname(DIR), "include")
-    return installed_path if os.path.exists(installed_path) else source_path
