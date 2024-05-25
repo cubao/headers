74a1f42c548cf296cd38700f33b269f188e7757f
TANG ZhiXiong <district10@users.noreply.github.com>
2024-05-25 23:57:16 +0800

    headers ok (#23)
    * update __init__.py
    
    * update
    
    * remove all __init__.py
    
    * fix
    
    * fix
    
    * fix
    
    * update
    
    * ready
    
    * update headers
    
    ---------
    
    Co-authored-by: TANG ZHIXIONG <zhixiong.tang@momenta.ai>

---
+++ build.py
@@ -0,0 +1,44 @@
+import os
+import glob
+from pathlib import Path
+
+head = '''#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+name = "cubao_headers"
+version = "0.0.3"
+
+pattern = ["*"]
+
+setup(
+    name=name,
+    version=version,'''
+print(head)
+
+os.chdir('cubao_headers/include')
+path = f'__init__.py'
+Path(path).touch()
+path = f'../__init__.py'
+Path(path).touch()
+packages = ['f"{name}"', 'f"{name}.include"']
+prefix = '        '
+
+dirs = sorted(glob.glob('**/*/', recursive=True))
+lines = []
+for dir in dirs:
+    package = 'f"{name}.include.' + dir[:-1].replace('/', '.') + '"'
+    packages.append(package)
+    line = f'{prefix}{package}: pattern,'
+    lines.append(line)
+    path = f'{dir}__init__.py'
+    Path(path).touch()
+
+print('    packages=[' + ',\n        '.join(packages) + '],')
+print('    package_data={')
+line = 'f"{name}.include": pattern,'
+print(f'{prefix}{line}')
+for line in lines:
+    print(line)
+tail = '''    },
+)'''
+print(tail)
