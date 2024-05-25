63b438f4103964f45a3f2dafc727c6cccfd56028
TANG ZHIXIONG <zhixiong.tang@momenta.ai>
2024-05-25 23:28:34 +0800

    fix

--- test.py
+++ test.py
@@ -2,12 +2,34 @@ import os
 import glob
 from pathlib import Path
 
-os.chdir('include')
+head = '''#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+name = "cubao_headers"
+version = "0.0.3"
+
+pattern = ["*"]
+
+setup(
+    name=name,
+    version=version,
+    packages=find_packages(),
+    package_data={'''
+print(head)
+
+os.chdir('src/cubao_headers/include')
+prefix = '        '
+line = 'f"{name}.include": pattern,'
+print(f'{prefix}{line}')
 dirs = sorted(glob.glob('**/*/', recursive=True))
 for dir in dirs:
     line = 'f"{name}.include.' + dir[:-1].replace('/', '.') + '": pattern,'
-    print(line)
+    print(f'{prefix}{line}')
     path = f'{dir}/__init__.py'
     if os.path.isfile(path):
         continue
     Path(path).touch()
+
+tail = '''    },
+)'''
+print(tail)
